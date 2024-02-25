cb34c7786a8831170288abf040398a69d8f7ddb7
kashryy <143761348+kashryy@users.noreply.github.com>
2024-02-25 03:30:51 +0200

    Add files via upload

---
+++ task1 EF/ConsoleApp9/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Task;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().Property(r => r.Date).IsRequired();
+            modelBuilder.Entity<Task>().ToTable("NewTask");
+            modelBuilder.Entity<Task>().Property(n => n.Date).HasColumnName("Deadline");
+        }
+
+    }
+}
---
+++ task1 EF/ConsoleApp9/Migrations/20240224141732_UpdateTaskModel.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp9.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaskModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "NewTask");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "NewTask",
+                newName: "Deadline");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "NewTask",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NewTask",
+                table: "NewTask",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NewTask",
+                table: "NewTask");
+
+            migrationBuilder.RenameTable(
+                name: "NewTask",
+                newName: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "Deadline",
+                table: "Customers",
+                newName: "Date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "Id");
+        }
+    }
+}
